2372688cc2b8c0c30d4771b307a934173dd3e21c
sowon <sowon.park@stonybrook.edu>
2023-03-15 14:42:12 +0900

    finish - Sowon Park

--- func.py
+++ func.py
@@ -8,9 +8,23 @@ def add(x, y):
 
 def neg(x):
     ... # fill here
+    return -x
 
 def sqrt(x):
     ... # fill here
+    return x**(1/2)
+
+def sub(x, y):
+    return x - y
+
+def div(x, y):
+    return x/y
+
+def mult(x, y):
+    return x*y
+
+def exp(x):
+    return x**2
 
 # -------------------------------------- Task 2 -----------------------------------
 
@@ -20,9 +34,9 @@ def sqrt(x):
 a = 1
 b = -3
 c = 1
-
-x1 = ... # TODO: write a code to compute the first root of the quadratic equation
-x2 = ... # TODO: then do the same for the second root
+d = sub(exp(b), mult(4,mult(a,c))) 
+x1 = div(add(neg(b), sqrt(d)), mult(2,a)) # TODO: write a code to compute the first root of the quadratic equation
+x2 = div(sub(neg(b), sqrt(d)), mult(2,a)) # TODO: then do the same for the second root
 # Note: Make sure to remove the ellipsis (...) when you're writing your code
 
 print("First root:" + str(x1))
